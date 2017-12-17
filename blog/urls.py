644c74d244143707fdb875ef1b7faad1a05aa038
Sylvia <sylviawei19950920@gmail.com>
2017-12-17 20:24:10 +0800

    static files

--- blog/urls.py
+++ blog/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from .views import homeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',homeView,name='home'),
     path('posts/', include('posts.urls',namespace='posts')),
 
 ]
